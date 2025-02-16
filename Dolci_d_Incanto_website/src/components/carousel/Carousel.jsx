de677e3506b296b403da8b9bedff0870b909f1f2
Marta <marta.ferraris25@gmail.com>
2025-02-16 18:43:19 +0100

    feat: carousel components and implement the carousel's logic that not responding

--- Dolci_d_Incanto_website/src/components/carousel/Carousel.jsx
+++ Dolci_d_Incanto_website/src/components/carousel/Carousel.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Carousel.css'
+import image1 from '../../assets/img/3.png'
+import image2 from '../../assets/img/4.png'
+import image3 from '../../assets/img/5.png'
 
 const Carousel = () => {
+
+
+
   return (
     <section>
-        
+      <div className='carousel-box'>
+        <div className='carousel'>
+          <img className='carousel-img' src={image1} alt="dolci" />
+          <img className='carousel-img' src={image2} alt="" />
+          <img className='carousel-img' src={image3} alt="" />
+        </div>
+      </div>
     </section>
   )
 }
